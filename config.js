ffdfd14739733924d89d312926d1934645c5d9bc
metacyber26-source <metacyber26@gmail.com>
2026-08-09 03:11:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+// Konfigurasi Alokasi Transaksi Nusantara Meta-Hub
+const SYSTEM_CONFIG = {
+  APP_NAME: "Nusantara Cultural Meta-Hub",
+  MANAGED_BY: "ICP2E Blitar Raya",
+  
+  // Skema Pembagian Hasil (Persentase)
+  REVENUE_SHARE: {
+    CREATOR_PERCENT: 0.50, // 50% Kreator/Partisipan
+    ORG_PERCENT: 0.30,     // 30% Kas ICP2E Blitar Raya
+    DEV_PERCENT: 0.20      // 20% Pengembang & Maintenance Server
+  },
+
+  // Alamat Dompet Pi (Ganti dengan Public Key Dompet Pi masing-masing)
+  WALLETS: {
+    ORG_WALLET: "GBBDAOMSTMI65E7Y462GXJ52XUWHKDUPSNEIAFYT72BKZBNZWEVVCYE7",
+    DEV_SERVER_WALLET: "GBBDAOMSTMI65E7Y462GXJ52XUWHKDUPSNEIAFYT72BKZBNZWEVVCYE7"
+  }
+};
+
