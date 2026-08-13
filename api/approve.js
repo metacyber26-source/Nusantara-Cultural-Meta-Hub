84a6c2e85fe85ef06d10c3a651cef8a85b1eb6a5
metacyber26-source <metacyber26@gmail.com>
2026-08-13 20:52:50 +0700

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // Izinkan Cross-Origin (CORS) dari Pi Browser
+  // Menangani masalah Cross-Origin (CORS) dari Pi Browser
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
 
     const API_KEY = process.env.PI_API_KEY;
     if (!API_KEY) {
-      console.error("PI_API_KEY tidak ditemukan di environment variables");
+      console.error("PI_API_KEY Missing on Vercel Environment Variables");
       return res.status(500).json({ error: 'PI_API_KEY Missing' });
     }
 
@@ -40,7 +40,7 @@ export default async function handler(req, res) {
     const data = await piRes.json();
 
     if (!piRes.ok) {
-      console.error("Pi Testnet Approval Failed:", data);
+      console.error("Pi Testnet Approval Error:", data);
       return res.status(piRes.status).json(data);
     }
 
